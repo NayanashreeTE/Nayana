73b0ac547906674e3978f72ddff6711d6462afa5
nayan <nayan@CPN862>
2024-08-15 14:24:46 +0530

    po

---
+++ src/Pro1/Pro2.java
@@ -0,0 +1,15 @@
+package Pro1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Pro2 {
+	@Test
+	public void test5() 
+	{
+		System.out.println("hii pooja");
+	}
+}
+
